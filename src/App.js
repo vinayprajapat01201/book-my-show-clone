db503e8ee10afe43f5e210f07f2da46b1eb6923b
vinayprajapat01201 <vinayprajapat01201@gmail.com>
2022-11-22 01:51:09 +0530

    updated pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+//Routing
+import { Routes, Route} from "react-router-dom";
+//React Slick css
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+// Pages
+import HomePage from './pages/Home_Page';
+import MoviePage from './pages/Movie_Page';
+import PlayPage from './pages/Play_Page';
 
 function App() {
-    return <div>
-            <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>;
+    return (
+    <Routes>
+      <Route path = "/" element={<HomePage/>} />
+      <Route path = "/movies/:id" element={<MoviePage/>} />
+      <Route path = "/plays" element = {<PlayPage/>}/>
+    </Routes>);
 }
 export default App;
