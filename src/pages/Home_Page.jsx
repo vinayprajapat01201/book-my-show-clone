44447559e338086b6b293cc7691676357be800f4
Vinay Prajapat <101922400+vinayprajapat01201@users.noreply.github.com>
2023-04-26 23:30:17 +0530

    Update Home_Page.jsx

--- src/pages/Home_Page.jsx
+++ src/pages/Home_Page.jsx
@@ -1,52 +1,83 @@
-import React,{useState} from 'react';
-//Components
-import EntertainmentCard from "../Components/Entertainment/EntertainmentCard_Component";
-import HeroCarousel from "../Components/HeroCarousel/HeroCarousel_Component";
-import PosterSlider from "../Components/PosterSlider/PosterSlider_Component";
-//Layout
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// Components
+import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.Component";
+import HeroCarousel from "../components/HeroCarousel/HeroCarousel.Component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+// Layout HOC
+import DefaultLayoutHoc from "../layout/Default.layout";
 
-import DefaultLayoutHoc from "../layout/Default_layout";
+const HomePage = () => {
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+  const [premierMovies, setPremierMovies] = useState([]);
+  const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
 
-const Homepage = () => {
-  const [recommendedMovies, setRecommendedMovies] =useState([]);
-  const [premierMovies, setPremierMovies] =useState([]);
-  const [onlineStreamEvents, setonlineStreamEvents] = useState([]);
+  useEffect(() => {
+    const requestPopularMovies = async () => {
+      const getPopularMovies = await axios.get("/movie/popular");
+      setRecommendedMovies(getPopularMovies.data.results);
+    };
 
+    requestPopularMovies();
+  }, []);
 
-  return( <>
-    <HeroCarousel/>
+  useEffect(() => {
+    const requestTopRatedMovies = async () => {
+      const getTopRatedMovies = await axios.get("/movie/top_rated");
+      setPremierMovies(getTopRatedMovies.data.results);
+    };
 
+    requestTopRatedMovies();
+  }, []);
 
-    <div className = "container mx auto px-4 md:px-12 my-8">
+  useEffect(() => {
+    const requestUpcomingMovies = async () => {
+      const getUpcomingMovies = await axios.get("/movie/upcoming");
+      setOnlineStreamEvents(getUpcomingMovies.data.results);
+    };
 
+    requestUpcomingMovies();
+  }, []);
+
+  return (
+    <>
+      <HeroCarousel />
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
         <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
           The best of Entertainment
         </h1>
-      <EntertainmentCard/>
-    </div>
-   <div className = "Container  mx auto px-4 md:px-12 my-8">
-   <PosterSlider
+        <EntertainmentCardSlider />
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
           title="Recommended Movies"
           subtitle="List of recommended movies"
           posters={recommendedMovies}
           isDark={false}
         />
-    </div>
-
-    <div className="bg-premier-800 py-12">
-      <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
-        <div className="hidden md:flex">
-          <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="Rupay" className="w-full h=full" />
-        </div>
-        <PosterSlider
-          title="Recommended Movies"
-          subtitle="Brand New releases every Friday"
-          posters={premierMovies}
-          isDark={true}
-        />
       </div>
-    </div>
-    <div className="container mx-auto px-4 md:px-12 my-8 ">
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img
+              src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              alt="Rupay"
+              className="w-full h-full"
+            />
+          </div>
+          <PosterSlider
+            title="Premiers"
+            subtitle="Brand new releases every Friday"
+            posters={premierMovies}
+            isDark={true}
+          />
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8 ">
         <PosterSlider
           title="Online Streaming Event"
           subtitle=""
@@ -54,10 +85,8 @@ const Homepage = () => {
           isDark={false}
         />
       </div>
-
-
     </>
-  )
-}
+  );
+};
 
-export default DefaultLayoutHoc(Homepage);
+export default DefaultLayoutHoc(HomePage);
